1e053aa296b74028bde668fb19bad9f8226a08fe
Hassan Shabbir <hassan149367@gmail.com>
2022-08-08 13:03:18 -0400

    02

--- solutions.js
+++ solutions.js
@@ -38,3 +38,46 @@ var findMaxConsecutiveOnes = function(nums) {
     }
     return maxOnes
 }
+
+/**
+ *
+Given an array nums of integers, return how many of them contain an even number of digits.
+
+Example 1:
+
+Input: nums = [12,345,2,6,7896]
+Output: 2
+Explanation: 
+12 contains 2 digits (even number of digits). 
+345 contains 3 digits (odd number of digits). 
+2 contains 1 digit (odd number of digits). 
+6 contains 1 digit (odd number of digits). 
+7896 contains 4 digits (even number of digits). 
+Therefore only 12 and 7896 contain an even number of digits.
+
+Example 2:
+
+Input: nums = [555,901,482,1771]
+Output: 1 
+Explanation: 
+Only 1771 contains an even number of digits.
+
+Constraints:
+
+    1 <= nums.length <= 500
+    1 <= nums[i] <= 105
+
+ * @param {number[]} nums
+ * @return {number}
+ *
+ * J solution: +/ 0= 2| #;.(_1) ' ',": 12 345 2 6 7896
+ */
+var findNumbers = function(nums) {
+    evenNums = 0
+    for (let num of nums) {
+        if (num.toString().length % 2 === 0) {
+            evenNums++
+        }
+    }
+    return evenNums
+};
