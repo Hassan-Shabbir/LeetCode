0d7900f7d3e8977b62f8e2e4ec92b837050ccfb0
Hassan Shabbir <hassan149367@gmail.com>
2022-08-10 22:58:22 -0400

    04

--- solutions.js
+++ solutions.js
@@ -129,3 +129,44 @@ var sortedSquares = function(nums) {
 }
 
 
+
+/**
+ *Given a fixed-length integer array arr, duplicate each occurrence of zero, shifting the remaining elements to the right.
+
+Note that elements beyond the length of the original array are not written. Do the above modifications to the input array in place ad do not return anything.
+
+Example 1:
+
+Input: arr = [1,0,2,3,0,4,5,0]
+Output: [1,0,0,2,3,0,0,4]
+Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
+
+Example 2:
+
+Input: arr = [1,2,3]
+Output: [1,2,3]
+Explanation: After calling your function, the input array is modified to: [1,2,3]
+
+Constraints:
+
+    1 <= arr.length <= 104
+    0 <= arr[i] <= 9
+
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ * J solution: {{ (#y) {. y#~ 1+ 0= y }} 1 0 2 3 0 4 5 0
+ */
+var duplicateZeros = function(arr) {
+    let len = arr.length
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == 0) {
+            for (let j = arr.length; j > i; j--) {
+                arr[j + 1] = arr[j]
+            }
+            arr[i+1] = 0
+            i++
+        }
+    }
+    arr.length = len
+}
+
