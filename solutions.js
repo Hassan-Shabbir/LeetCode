0c964d4c2e70a7ef8637aacb4f4526dae408667f
Hassan Shabbir <hassan149367@gmail.com>
2022-08-16 17:53:26 -0400

    09

--- solutions.js
+++ solutions.js
@@ -407,3 +407,65 @@ var checkIfExist = function(arr) {
     }
     return false
 }
+
+/**
+ *
+  Valid Mountain Array
+
+Given an array of integers arr, return true if and only if it is a valid mountain array.
+
+Recall that arr is a mountain array if and only if:
+
+    arr.length >= 3
+    There exists some i with 0 < i < arr.length - 1 such that:
+        arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+        arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+
+Example 1:
+
+Input: arr = [2,1]
+Output: false
+
+Example 2:
+
+Input: arr = [3,5,5]
+Output: false
+
+Example 3:
+
+Input: arr = [0,3,2,1]
+Output: true
+
+Constraints:
+
+    1 <= arr.length <= 104
+    0 <= arr[i] <= 104
+
+ * @param {number[]} arr
+ * @return {boolean}
+ * J solution: TODO
+ */
+var validMountainArray = arr => {
+    let increasing = true
+    let switched = 0
+    let valid = true
+    if (arr.length < 3) { 
+        return false 
+    }
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] > arr[i+1] && i == 0) {
+            valid = false
+        }
+        if (arr[i] > arr[i+1] && i != 0) {
+            increasing = false
+        }
+        
+        if (increasing) {
+            valid = valid && (arr[i] < arr[i+1])
+        }
+        else if (!increasing) {
+            valid = valid && (arr[i] > arr[i+1])
+        }
+    }
+    return valid && !increasing
+}
