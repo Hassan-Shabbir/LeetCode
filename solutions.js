55c302099168da077c825939397a554e9e22b4d3
Hassan Shabbir <hassan149367@gmail.com>
2022-08-30 17:43:23 -0400

    19

--- solutions.js
+++ solutions.js
@@ -791,3 +791,65 @@ var heightChecker = heights => {
     }
     return diffs
 }
+
+
+/**
+ *  Third Maximum Number
+
+Given an integer array nums, return the third distinct maximum number in this array. If the third maximum does not exist, return the maximum number.
+
+Example 1:
+
+Input: nums = [3,2,1]
+Output: 1
+Explanation:
+The first distinct maximum is 3.
+The second distinct maximum is 2.
+The third distinct maximum is 1.
+
+Example 2:
+
+Input: nums = [1,2]
+Output: 2
+Explanation:
+The first distinct maximum is 2.
+The second distinct maximum is 1.
+The third distinct maximum does not exist, so the maximum (2) is returned instead.
+
+Example 3:
+
+Input: nums = [2,2,3,1]
+Output: 1
+Explanation:
+The first distinct maximum is 3.
+The second distinct maximum is 2 (both 2's are counted together since they have the same value).
+The third distinct maximum is 1.
+
+Constraints:
+
+    1 <= nums.length <= 104
+    -231 <= nums[i] <= 231 - 1
+ 
+Follow up: Can you find an O(n) solution?
+ * @param {number[]} nums
+ * @return {number}
+ * J solution: {{ 2 } (,{.)^:(2) ~. \:~ y }}
+ */
+var thirdMax = nums => {
+    let maxes = [-Infinity, -Infinity, -Infinity];
+    let len = 0;
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (nums[i] > maxes[j] && nums[i] != maxes[0] && nums[i] != maxes[1] && nums[i] != maxes[2]) {
+                maxes.splice(j, 0, nums[i]);
+                len++;
+                break;
+            }
+        }
+    }
+    if (len >= 3) { 
+        return maxes[2];
+    } else { 
+        return maxes[0];
+    }
+}
