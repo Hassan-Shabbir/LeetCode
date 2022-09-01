963847bf131c2bd80607c5b9bd2d267ad7f74b9c
Hassan Shabbir <hassan149367@gmail.com>
2022-08-31 22:48:10 -0400

    20

--- solutions.js
+++ solutions.js
@@ -853,3 +853,48 @@ var thirdMax = nums => {
         return maxes[0];
     }
 }
+
+/**
+ *
+  Find All Numbers Disappeared in an Array
+
+Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+Example 1:
+
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+
+Example 2:
+
+Input: nums = [1,1]
+Output: [2]
+
+Constraints:
+
+    n == nums.length
+    1 <= n <= 105
+    1 <= nums[i] <= n
+
+Follow up: Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+ * @param {number[]} nums
+ * @return {number[]}
+ * J solution: {{ >: I. -. y e.~ >: i. # y }} 4 3 2 7 8 2 3 1
+ */
+var findDisappearedNumbers = nums => {
+    let missing = [];
+    for (let i = 1; i <= nums.length; i++) {
+        let found = 0;
+        let j = 0;
+        for (; j < nums.length; j++) {
+            if (i === nums[j]) {
+                found = 1;
+                break;
+            }
+        }
+        if (!found) {
+            missing.push(i);
+        }
+    }
+    return missing;
+}
