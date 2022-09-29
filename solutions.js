724c295d938184f5c6bf609ff49be567f41ec840
Hassan Shabbir <hassan149367@gmail.com>
2022-09-28 21:38:22 -0400

    29

--- solutions.js
+++ solutions.js
@@ -1159,16 +1159,54 @@ Constraints:
  * @return {number}
  * J solution: {{ +/ , x =/ y }}
  */
-var numJewelsInStones = function(jewels, stones) {
-    let sum = 0
-    for (let jewel of jewels) {
-        for (let stone of stones) {
-            if (jewel === stone) {
-                sum++
-            }
+var numJewelsInStones = (jewels, stones) => {
+  let jewelSet = new Set(jewels)
+	let sum = 0
+
+	for (let stone of stones) {
+		if (jewelSet.has(stone)) {
+			sum++
+		}
+	}
+}
+
+
+/**
+ *
+392. Is Subsequence
+Easy
+
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+Example 1:
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
+
+Constraints:
+
+    0 <= s.length <= 100
+    0 <= t.length <= 104
+    s and t consist only of lowercase English letters.
+ 
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = (s, t) => {
+    let sp = 0
+    
+    for (let tp = 0; tp < t.length; tp++) {
+        if (t[tp] === s[sp]) {
+            sp++
         }
     }
-    return sum
-};
-
-
+    return sp === s.length
+}
